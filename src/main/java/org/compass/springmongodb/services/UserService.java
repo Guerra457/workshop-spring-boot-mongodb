baaba2e7e5b9620488141932fd80bf5900b5b602
Matheus Guerra <matheus4xp@gmail.com>
2025-01-24 16:46:28 -0300

    Obtendo um usuário por id

--- src/main/java/org/compass/springmongodb/services/UserService.java
+++ src/main/java/org/compass/springmongodb/services/UserService.java
@@ -2,6 +2,7 @@ package org.compass.springmongodb.services;
 
 import org.compass.springmongodb.domain.User;
 import org.compass.springmongodb.repository.UserRepository;
+import org.compass.springmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,13 @@ public class UserService {
 
     @Autowired
     private UserRepository repo;
+
     public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
