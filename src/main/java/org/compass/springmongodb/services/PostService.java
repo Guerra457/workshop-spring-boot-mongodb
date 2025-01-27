95a940b1cf30e1ae8627f196b3e842a916df49a7
Matheus Guerra <matheus4xp@gmail.com>
2025-01-27 16:52:07 -0300

    Obtendo um post por id

---
+++ src/main/java/org/compass/springmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package org.compass.springmongodb.services;
+
+import org.compass.springmongodb.domain.Post;
+import org.compass.springmongodb.domain.User;
+import org.compass.springmongodb.dto.UserDTO;
+import org.compass.springmongodb.repository.PostRepository;
+import org.compass.springmongodb.repository.UserRepository;
+import org.compass.springmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
