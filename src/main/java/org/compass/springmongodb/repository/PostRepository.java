800b49f8593b1773941e240fb517011f3ce59185
Matheus Guerra <matheus4xp@gmail.com>
2025-01-27 16:29:57 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/org/compass/springmongodb/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package org.compass.springmongodb.repository;
+
+import org.compass.springmongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
