b0ad89ee02b7e05e210044391d595f89df71cb3d
Matheus Guerra <matheus4xp@gmail.com>
2025-01-27 17:00:48 -0300

    Acrescentando comentarios aos posts

--- src/main/java/org/compass/springmongodb/domain/Post.java
+++ src/main/java/org/compass/springmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package org.compass.springmongodb.domain;
 
 import org.compass.springmongodb.dto.AuthorDTO;
+import org.compass.springmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,6 +19,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {}
 
@@ -66,6 +71,15 @@ public class Post implements Serializable {
     public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
